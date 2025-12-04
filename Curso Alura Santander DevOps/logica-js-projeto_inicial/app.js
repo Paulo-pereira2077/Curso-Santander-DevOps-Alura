45c354084a3ffbd7a5f6604803dff559886c129a
Paulo <pcrrp.pereira@gmail.com>
2025-12-04 14:34:39 -0300

    Primeiro desafio do curso.

--- Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
+++ Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
@@ -1,11 +1,19 @@
-alert('Boas-vindas ao jogo do Número Secreto');
-let numeroSecreto = 29;
+alert("Boas vindas ao nosso site!");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
 
-let chute = prompt('Escolha um número entre 1 e 30');
+alert("Erro! Preencha todos os campos")
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
 
-if (numeroSecreto == chute){
-    console.log('Isso ai! Você descobriu o número secreto (29)');
+let nomeU = prompt("Digite seu nome: ");
+let idadeU = prompt("Digite sua idade: ");
+
+if(idadeU >= 18){
+    alert("Pode tirar a habilitação!");
 }
 else{
-    console.log('Que pena! O número secreto é 29. Tente novamente!');
+    alert("Não pode tirar a habilitação!");
 }
