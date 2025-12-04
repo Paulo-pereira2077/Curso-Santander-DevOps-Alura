119f9f12b9c2beb6c82b0283d078952cc0c62fd8
Paulo <pcrrp.pereira@gmail.com>
2025-12-04 16:02:16 -0300

    Aulas 3, 4, 5 e 6

--- Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
+++ Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
@@ -1,11 +1,29 @@
 alert('Boas-vindas ao jogo do Número Secreto');
-let numeroSecreto = 29;
+let numeroSecreto = parseInt (Math.random() * 100 + 1);
+console.log(numeroSecreto); 
 
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute;
 
-if (numeroSecreto == chute){
-    console.log(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+let tentativas = 1;
+
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+
+    if (chute == numeroSecreto) {        
+        break;
+    }
+    else {        
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas ++;
+    }
+
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';    
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 }
-else {
-    alert('Você errou :(');
-}
+
