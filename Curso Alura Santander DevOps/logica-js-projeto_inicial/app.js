13dae18fea41814e1a4dc92dc11d1d157c4401ad
Paulo <pcrrp.pereira@gmail.com>
2025-12-03 23:33:15 -0300

    Testando condições

--- Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
+++ Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
@@ -6,3 +6,6 @@ let chute = prompt('Escolha um número entre 1 e 30');
 if (numeroSecreto == chute){
     console.log('Isso ai! Você descobriu o número secreto (29)');
 }
+else{
+    console.log('Que pena! O número secreto é 29. Tente novamente!');
+}
