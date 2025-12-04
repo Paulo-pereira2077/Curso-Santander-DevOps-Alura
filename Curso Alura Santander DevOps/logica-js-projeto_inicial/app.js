29b9c527abde01037eca825e40b08f994432f7d9
Paulo <pcrrp.pereira@gmail.com>
2025-12-04 16:58:32 -0300

    Aulas 2 e 3 - Iniciando conceitos de funções e manipulações de JS com html

--- Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
+++ Curso Alura Santander DevOps/logica-js-projeto_inicial/app.js
@@ -1,31 +1,9 @@
-// alert('Boas-vindas ao jogo do Número Secreto');
-// let numeroMaximo = 5000;
-// let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-// console.log(numeroSecreto); 
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto'; 
 
-// let chute;
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-// let tentativas = 1;
-
-
-// while (chute != numeroSecreto) {
-//     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-
-//     if (chute == numeroSecreto) {        
-//         break;
-//     }
-//     else {        
-//         if (chute > numeroSecreto) {
-//             alert(`O número secreto é menor que ${chute}`);
-//         }
-//         else {
-//             alert(`O número secreto é maior que ${chute}`);
-//         }
-//         tentativas ++;
-//     }
-// }
-
-//     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';    
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-
-
+function verificarChute() {
+    console.log('O botão foi clicado')
+}
