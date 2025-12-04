4dfa632115f88f34b0166ee7ec3408588e3e2233
Paulo <pcrrp.pereira@gmail.com>
2025-12-04 16:46:21 -0300

    Atualizando o projeto para o curso de Lógica de programação: explore funçoes e listas

--- Curso Alura Santander DevOps/logica-js-projeto_inicial/img/JS Game_files/app.js
+++ Curso Alura Santander DevOps/logica-js-projeto_inicial/img/JS Game_files/app.js
@@ -1,22 +1,31 @@
-// let numeroSecreto = parseInt(Math.random() * 11)
-// let tentativas = 1
-// let chute
+// alert('Boas-vindas ao jogo do Número Secreto');
+// let numeroMaximo = 5000;
+// let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+// console.log(numeroSecreto); 
 
-// alert('Boas vindas ao jogo do número secreto')
+// let chute;
+
+// let tentativas = 1;
+
 
 // while (chute != numeroSecreto) {
-//     let chute = prompt('Escolha um número entre 1 e 10')
-//     if (chute == numeroSecreto) {
-//         break
-//     } else {
-//         if (chute > numeroSecreto) {
-//             alert('O número secreto é menor')
-//         } else {
-//             alert('O número secreto é maior')
-//         }
+//     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+//     if (chute == numeroSecreto) {        
+//         break;
 //     }
-//     tentativas++
+//     else {        
+//         if (chute > numeroSecreto) {
+//             alert(`O número secreto é menor que ${chute}`);
+//         }
+//         else {
+//             alert(`O número secreto é maior que ${chute}`);
+//         }
+//         tentativas ++;
+//     }
 // }
 
-// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
-// alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
+//     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';    
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+
