adbd80532b42c8f1c9de088314e5e0196b681ea5
Paulo <pcrrp.pereira@gmail.com>
2025-12-08 22:25:47 -0300

    Finalizando o projeto, adicionando a função de limpar os campos de digitação e função de reiniciar o jogo

--- Curso Alura Santander DevOps/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ Curso Alura Santander DevOps/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,26 +1,53 @@
-function sortear(){
+function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    
+
     let sorteados = [];
     let numero;
 
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
-        
+
         while (sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate);
         }
-        
+
         sorteados.push(numero);
-}
+    }
 
     let resultado = document.getElementById('resultado')
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;    
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+
+    document.getElementById('btn-reiniciar').classList.remove('container__botao-desabilitado');
+    document.getElementById('btn-reiniciar').classList.add('container__botao');
+
+    document.getElementById('btn-sortear').setAttribute('disabled', true);
+
 
 }
 
 function obterNumeroAleatorio(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
+
+    document.getElementById('btn-reiniciar').classList.add('container__botao-desabilitado');
+    document.getElementById('btn-reiniciar').classList.remove('container__botao');
+
+    document.getElementById('btn-sortear').removeAttribute('disabled');
+}
+
+function limparCampos() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
 }
